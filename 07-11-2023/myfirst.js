edfe7831a9284078bcbc6bc99ad2013d6ddaef58
Bilal Jhandho <bjhandho@gmail.com>
2023-11-07 12:29:23 +0500

    javatasks

---
+++ 07-11-2023/myfirst.js
@@ -0,0 +1,55 @@
+// console.log("jandho");
+// console.log(typeof(2));
+
+// console.log(typeof(true));
+// console.log(typeof(a));
+// console.log(2+5);
+
+// var currentTime=10;
+// console.log(currentTime>22 && currentTime<17);
+
+// var currentTime=7;
+// console.log(currentTime<9 || currentTime>17);
+
+// console.log(22%5); //2
+
+// console.log(typeof(22%5));
+
+// console.log(22%5);
+// var petHungry=true;
+// petHungry=!petHungry;
+// console.log(typeof(petHungry));
+
+// console.log(22%5);
+// var petHungry=true;
+// petHungry=!petHungry;
+// console.log(5==5);
+// console.log(5!=5);
+// console.log(365+days);
+
+// var overtime=1;
+// overtime+=2;
+// overtime+=1;
+// overtime+=2;
+// overtime+=3;
+// console.log(overtime);
+
+// var longString="";
+// longString+="Once     ";
+// longString+="upon ";
+// longString+="a ";
+// longString+="time";
+// longString+="...";
+// console.log(longString);
+
+// var score=8;
+// console.log("Mid-level skills:",>0score<10 && score<17);
+
+var num1=2;
+var num2=5;
+var test1=num1%2;
+var test2=num2%2;
+var result1=test1==0;
+var result2=test2==0;
+console.log("is",num1,"an even number?",result1);
+console.log("is",num2,"an even number?",result2);
