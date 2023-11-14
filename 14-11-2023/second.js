b4621d26c844fa8745722c7846362bdceb56e48e
Bilal Jhandho <bjhandho@gmail.com>
2023-11-14 09:15:07 +0500

    quiz java

---
+++ 14-11-2023/second.js
@@ -0,0 +1,45 @@
+// var result=40;
+// if(result>40){
+//     console.log("Congratulations you are passed");
+// }else{
+//     console.log("Unfortunately you did'nt pass")
+// }
+
+
+// var food="cold";
+// if(food=="hot"){
+//     console.log('Too hot')
+// }
+// else if(food=="cold"){
+//     console.log('too cold')
+// }
+// else{
+//     console.log('just right')
+// }
+
+//switch statement
+// var place='second';
+// switch(place){
+//     case'first':
+//     console.log('Gold');
+//     break;
+//     case'second':
+//     console.log('silver');
+//     break;
+//     case'third':
+//     console.log('bronze');
+//     break;
+//     default:
+//         console.log('no medal');
+// }
+
+var age=10;
+if(age>=65){
+    console.log('you will get your pension at first age')
+}
+else if(age<65 && age>=18){
+    console.log('each month you get a salary')
+}
+else{
+    console.log('you will not get a salary')
+}
