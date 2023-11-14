f83d65a3ad848b93f42f8d977cef505b525957be
Bilal Jhandho <bjhandho@gmail.com>
2023-11-14 10:30:02 +0500

    java quiz

--- 14-11-2023/second.js
+++ 14-11-2023/second.js
@@ -33,13 +33,71 @@
 //         console.log('no medal');
 // }
 
-var age=10;
-if(age>=65){
-    console.log('you will get your pension at first age')
-}
-else if(age<65 && age>=18){
-    console.log('each month you get a salary')
-}
-else{
-    console.log('you will not get a salary')
+// var age=10;
+// if(age>=65){
+//     console.log('you will get your pension at first age')
+// }
+// else if(age<65 && age>=18){
+//     console.log('each month you get a salary')
+// }
+// else{
+//     console.log('you will not get a salary')
+// }
+
+
+// var day = 'sunday';
+// switch(day){
+//     case'sunday':
+//     console.log('last day of week');
+//     break;
+//     case'monday':
+//     console.log('first day of week');
+//     break;
+//     case'tuesday':
+//     console.log('second day of week');
+//     break;
+//     case'wednesday':
+//     console.log('third day of week');
+//     case'thrusday':
+//     console.log('4th day of week');
+//     case'friday':
+//     console.log('5th day of week');
+//     case'satuday':
+//     console.log('6th day of week');
+//     default:
+//         console.log('no day');
+// }
+
+
+//   loops
+// for(var i=1 ;i<4; i++){
+//     console.log(i);
+// }
+
+// var i=1;
+// while(i<5){
+//     console.log('BILAL Asif')
+//     i=i+1;
+// 
+
+// for(i=1; i<=2 ;i++){
+//     for(j=1; j<=5 ;j++){
+//         console.log("Week" + i + "day" +j);
+//     }
+// }
+
+for(var i=1; i<=10; i++){
+    switch (i){
+        case 1:
+        console.log('gold medal')
+        break
+        case 2:
+            console.log('silver medal')
+        break
+        case 3:
+            console.log('Bronze medal')
+        break
+        default:
+            console.log(i)
+    }
 }
